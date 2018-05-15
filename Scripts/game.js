0d1cdaeb0f6ca6cbfc5e0fb71afba5135aca460b
DannyRux <dangorlov@yahoo.com>
2018-05-15 10:18:08 -0400

    Linter errors fixed

--- Scripts/game.js
+++ Scripts/game.js
@@ -1,8 +1,17 @@
+/// GAME.JS
+/// Daniel Orlovsky
+/// Brick Breaker Clone with high score leaderboard.
+
+
 'use strict';
 
+
+// Clamps a number between a min and a max
 function clamp(num, min, max) {
     return num <= min ? min : num >= max ? max : num;
 }
+
+// Colors each row of bricks will be
 var rowColors = [
     'green',
     'red',
@@ -12,19 +21,26 @@ var rowColors = [
 ];
 
 
-
+// Our reference to the canvas element
 var canvas = document.getElementById('breakoutCanvas');
+
+// The context to which we will draw
 var ctx = canvas.getContext("2d");
 
+
+/// INITIAL BALL VALUES
 var ballX = paddleX + (paddleWidth / 2); //canvas.width / 2;
 var ballY = paddleY - ballRadius; //canvas.height / 2;
 var ballMoving = false;
 var ballRadius = 9;
 var ballTrail = [];
 var ballTrailMax = 20;
+
+// Initial velocity for the direction of the ball.
 var dx = 1.5;
 var dy = -4;
 
+/// PADDLE VALUES
 var paddleHeight = 10;
 var paddleWidth = 100;
 var paddleX = (canvas.width - paddleWidth) / 2;
@@ -32,9 +48,12 @@ var paddleY = canvas.height - (canvas.height / 10);
 var paddleCurve = 1.5;
 
 
+/// DIRECTIONAL INPUT FLAGS
 var rightPressed = false;
 var leftPressed = false;
 
+
+/// INITAL LEVEL VALUES FOR BRICKS
 var brickRowCount = 5;
 var brickColumnCount = 8;
 var brickWidth = 75;
@@ -44,17 +63,22 @@ var brickOffsetTop = 30;
 var brickOffsetLeft = (canvas.width - ((brickWidth + brickPadding) * brickColumnCount)) / 2;
 var bricks = [];
 
+
+/// STAR FILED VALUES
 var frontStarField = [];
 var rearStarField = [];
 const maxStars = 250;
 
+/// TIMER REFERENCES
 var gamingLoop;
 var timerLoop;
 
-
+/// SOUNDS
 var paddleHit = '../sounds/paddleHit.wav';
 var brickSmash = '../sounds/brickSmash.wav';
 
+
+/// INITIAL GAME VALUES
 var playerScore = 0;
 var finalScore = 0;
 var timer = 60;
@@ -62,6 +86,7 @@ var timer = 60;
 var modalUp = false;
 
 
+/// DEPRECATED
 function showLoadingScreen() {
     var body = document.body;
     var html = document.documentElement;
@@ -79,7 +104,7 @@ function hideLoadingScreen() {
 };
 
 
-
+/// RESETS THE BRICK LAYOUT
 function resetBricks() {
     for (var c = 0; c < brickColumnCount; c++) {
         bricks[c] = [];
@@ -97,11 +122,14 @@ function resetBricks() {
     }
 }
 
+/// ADDS OUR APPROPRIATE EVENT LISTENERS
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 document.getElementById("savePlayer").addEventListener("click", savePlayer, false);
 document.getElementById("modalClose").addEventListener("click", modalClose, false);
 
+
+/// SAVES THE PLAYERS SCORE THE THE DATABASE
 function savePlayer() {
     modalUp = false;
     var playerName = document.getElementById("playerName").value.trim();
@@ -128,19 +156,26 @@ function modalClose() {
     modalUp = false;
 }
 
+/// 
 $('#saveScore').on('hidden.bs.modal', function () {
     modalClose();
 })
+
+
 function keyDownHandler(e) {
+    /// DO NOTHING IF WE ARE NOT IN THE GAME
     if (modalUp) return;
 
-    if (e.keyCode == 39 || e.keyCode == 68) {
+    // CHECKS FOR 'D' or RIGHT ARROW
+    if (e.keyCode === 39 || e.keyCode === 68) {
         rightPressed = true;
-    } else if (e.keyCode == 37 || e.keyCode == 65) {
+    /// CHECKS FOR 'A' or LEFT ARROW
+    } else if (e.keyCode === 37 || e.keyCode === 65) {
         leftPressed = true;
     }
+    // IF THE BALL IS NOT MOVING, WE CHECK IF THE SPACEBAR IS PRESSED.  THIS SETS BALLMOVING TO TRUE AND STARTS THE TIMER
     if (!ballMoving) {
-        if (e.keyCode == 32) {
+        if (e.keyCode === 32) {
             e.preventDefault();
             document.getElementById("gameover").style.display = "none";
             document.getElementById("bonusDisplay").style.display = "none";
@@ -150,17 +185,18 @@ function keyDownHandler(e) {
     }
 }
 
+// CHECKS TO FLAG IF THE PADDLE SHOULD NO LONGER BE MOVING
 function keyUpHandler(e) {
     if (modalUp) return;
-    if (e.keyCode == 39 || e.keyCode == 68) {
+    if (e.keyCode === 39 || e.keyCode === 68) {
         rightPressed = false;
-    } else if (e.keyCode == 37 || e.keyCode == 65) {
+    } else if (e.keyCode === 37 || e.keyCode === 65) {
         leftPressed = false;
     }
 
 }
 
-
+// TO PLAY A SOUND, WE CREATE AN AUDIO ELEMENT AND SUBSCRIBE TO THE ended EVENT, AND DESTROY THE ELEMENT AT THAT TIME
 function playSound(soundPath) {
     // creates an audio element (need jquery)
     var audioElement = document.createElement("audio"); //$('<audio></audio>').src = audioFile;
@@ -174,12 +210,12 @@ function playSound(soundPath) {
     audioElement.play();
 }
 
-
+/// DRAWS OUR LEVEL.  A BRICK STATUS OF 1 MEANS IT SHOULD BE DISPLAYED AND CAN BE HIT
 function drawBricks() {
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
             var currBrick = bricks[c][r];
-            if (currBrick.status == 1) {
+            if (currBrick.status === 1) {
                 ctx.beginPath();
                 ctx.rect(currBrick.x, currBrick.y, brickWidth, brickHeight);
                 ctx.fillStyle = currBrick.color;
@@ -190,20 +226,22 @@ function drawBricks() {
     }
 }
 
+/// CHECKS IF THERE IS ONE BRICK ON THE SCREEN WITH A STATUS OF 1 - IF SO, THE GAME IS STILL IN SESSION
 function checkWin() {
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
-            if (bricks[c][r].status == 1) return false;
+            if (bricks[c][r].status === 1) return false;
         }
     }
     return true;
 }
 
+/// CHECKS FOR A COLLISSION WITH ANY BRICKS ON THE SCREEN
 function collisionDetection() {
     for (var c = 0; c < brickColumnCount; c++) {
         for (var r = 0; r < brickRowCount; r++) {
             var b = bricks[c][r];
-            if (bricks[c][r].status == 1) {
+            if (bricks[c][r].status === 1) {
                 if (ballX + ballRadius > b.x && ballX - ballRadius < b.x + brickWidth && 
                     ballY + ballRadius > b.y && ballY - ballRadius < b.y + brickHeight) {
                     dy *= -1;
@@ -251,6 +289,7 @@ function collisionDetection() {
     }
 }
 
+/// SIMPLY DRAWS A PIXEL FOR THE STAR FIELD
 function drawPixel(x, y, color, size=1) {
     ctx.beginPath();
     ctx.rect(x, y, size, size);
@@ -258,13 +297,18 @@ function drawPixel(x, y, color, size=1) {
     ctx.fill();
     ctx.closePath();
 }
+
+/// DRAWS A LARGER CIRCLE FOR THE BALL TRACER
 function drawTracerTrail(x, y, color) {
     ctx.beginPath();
     ctx.arc(x, y, 3, 0, Math.PI * 2);
     ctx.fillStyle = color;
     ctx.fill();
 }
+
+/// DRAWS THE TRAIL THAT FOLLOWS THE BALL
 function drawBallTrail() {
+    // COLORS INCREMENTALLY GET LIGHTER
     var colors = [
         '#7D6608',
         '#B7950B',
@@ -276,6 +320,8 @@ function drawBallTrail() {
         drawTracerTrail(ballTrail[i].x, ballTrail[i].y, colors[i / 4]);
     }
 }
+
+/// DRAWS OUR BALL
 function drawBall() {
     drawBallTrail();
     ctx.beginPath();
@@ -286,11 +332,13 @@ function drawBall() {
     
 }
 
+/// UPDATES TIMER and SCORE
 function updateHUD() {
     document.getElementById("bonusTimer").innerHTML = timer;
     document.getElementById("score").innerHTML = playerScore;
 }
 
+/// DRAWS OUR PADDLE
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
@@ -299,6 +347,7 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+/// HANDLES MOVING THE PADDLE BASED ON INPUT
 function handleInput() {
     if (rightPressed) {
         paddleX += 8;
@@ -308,10 +357,13 @@ function handleInput() {
     paddleX = clamp(paddleX, 0, canvas.width - paddleWidth);
 }
 
+// THESE VALUES ARE FOR THE requestAnimationFrame feature of the HTML5 canvas
+// Allows for smoother and more consistent gameplay.
 let maxFPS = 60;
 let lastFrameTimeMs = 0;
 let timeStep = 1000 / 60;
 
+// Draws everything on the screen
 function drawAssets() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawfrontStarField();
@@ -320,6 +372,14 @@ function drawAssets() {
     drawPaddle();
 }
 
+/// THE MAIN GAME LOOP
+// WE:
+// CHECK FOR INPUT
+// UPDATE THE Heads Up Display
+// Update the background
+// See if we've won on the last time the loop iterated (HANDLE)
+// Check for boundary collisions
+// Draw Assets
 function gameLoop(timestamp) {
     
     handleInput();
@@ -372,6 +432,7 @@ function gameLoop(timestamp) {
 
 }
 
+/// Our bonus countdown timer
 function countDown() {
     timer--;
     if (timer <= 0) {
@@ -379,6 +440,7 @@ function countDown() {
     }
 }
 
+// Gets scores for the leaderboard
 function getScores(callback) {
     $.ajax({
         url: 'Home/GetScores',
@@ -389,6 +451,8 @@ function getScores(callback) {
     })
 }
 
+
+/// Initializes the start fields with values
 function initfrontStarField() {
     frontStarField = [];
     rearStarField = [];
@@ -405,6 +469,7 @@ function initfrontStarField() {
 
 }
 
+// Updates the front and rear starfield (these stars travel faster) - need to change the name
 function updatefrontStarField() {
     for (var i = 0; i < frontStarField.length; i++) {
         frontStarField[i].y += 2;
@@ -421,7 +486,7 @@ function updatefrontStarField() {
 }
 
 
-
+// Draws the star field
 function drawfrontStarField() {
     for (var i = 0; i < frontStarField.length; i++) {
         drawPixel(frontStarField[i].x, frontStarField[i].y, "white");
@@ -429,6 +494,7 @@ function drawfrontStarField() {
     }
 }
 
+/// RESETS DEFAULT VALUES
 function resetGame() {
     showLoadingScreen();
     ballMoving = false;
